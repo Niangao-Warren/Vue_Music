07ef40be2fead6df4ac6830456a5d7fdadb564b3
Niangao.Warren <503809815@qq.com>
2018-04-02 10:57:22 +0800

    Vue_Music_01

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+	import MHeader from 'components/m-header/m-header'
+	
+	export default {
+		components: {
+			MHeader
+		}
+	}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
