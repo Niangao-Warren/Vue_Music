aef81a0665dec778e71c4266b0bc4fb1a39640ea
Niangao-Warren <503809815@qq.com>
2018-07-17 10:05:29 +0800

    Vue_Music_02

---
+++ Vue_Music_02/src/main.js
@@ -0,0 +1,15 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+import './common/stylus/index.styl'
+
+fastclick.attach(document.body)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
